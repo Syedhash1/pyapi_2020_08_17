601245ffd903e847e9913c2359aaba6257968d99
Syedhash1 <sunny.hash@gmail.com>
2020-08-20 19:23:44 +0000

    flask and jinja lab

---
+++ pyapi/flaskapi/jinja2temp02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def default():
+    return "You are at the default route"
+
+#grab the value 'username'
+@app.route("/<username>")
+def index(username):
+    # render the jinja template "helloname.html"
+    # apply the value of username for the var name
+    return render_template("helloname.html", name = username)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
+
